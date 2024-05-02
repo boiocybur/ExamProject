5d76be6729f30cec60edfef12b533a7b9cae9eab
Jesper Deetho Jessen <jeje0002@stud.kea.dk>
2024-05-02 14:21:10 +0200

    Project setup done

--- src/main/java/com/example/examproject/controller/ProjectController.java
+++ src/main/java/com/example/examproject/controller/ProjectController.java
@@ -1,3 +1,20 @@
 package com.example.examproject.controller;
+
+import com.example.examproject.model.Project;
+import com.example.examproject.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Controller
+@RequestMapping("")
 public class ProjectController {
+    private final ProjectService projectService;
+
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 }
