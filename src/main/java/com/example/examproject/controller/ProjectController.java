5b61d41a708997dfc8f512a6975919821b5dbc9f
Jesper Deetho Jessen <jeje0002@stud.kea.dk>
2024-05-13 11:00:27 +0200

    ProjectController klasse fikset

--- src/main/java/com/example/examproject/controller/ProjectController.java
+++ src/main/java/com/example/examproject/controller/ProjectController.java
@@ -1,8 +1,6 @@
 package com.example.examproject.controller;
 
-import com.example.examproject.model.Project;
 import com.example.examproject.service.ProjectService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,33 +17,14 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
-    @GetMapping("/imminent")
-    public ResponseEntity<List<Project>> getImminentProjects() {
-        List<Project> projects = projectService.findProjectsByImminentDeadlines();
-        return ResponseEntity.ok(projects);
-    }
-
-    @GetMapping("/overdue")
-    public ResponseEntity<List<Project>> getOverdueProjects() {
-        List<Project> projects = projectService.findOverdueProjects();
-        return ResponseEntity.ok(projects);
-    }
-
-    @GetMapping("/completed")
-    public ResponseEntity<List<Project>> getCompletedProjects() {
-        List<Project> projects = projectService.findCompletedProjects();
-        return ResponseEntity.ok(projects);
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<List<Project>> getAllProjects(){
-        List<Project> projects = projectService.findAllProjects();
-        return ResponseEntity.ok(projects);
-    }
-
-    @GetMapping("/dashboard")
+    @GetMapping("dashboard")
     public String dashboard(Model model) {
-        return "dashboard-task";
+        model.addAttribute("imminentProjects", projectService.findProjectsByImminentDeadlines());
+        model.addAttribute("overdueProjects", projectService.findOverdueProjects());
+        model.addAttribute("completedProjects", projectService.findCompletedProjects());
+        model.addAttribute("allProjects", projectService.findAllProjects());
+        return "dashboard";
     }
+
 
 }
