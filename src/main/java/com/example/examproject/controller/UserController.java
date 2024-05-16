c6680117bf6fdf668b8ef4fad9c114c0b7e25a73
Oskar <osth0002@stud.kea.dk>
2024-05-16 10:10:04 +0200

    projectCreation p2

--- src/main/java/com/example/examproject/controller/UserController.java
+++ src/main/java/com/example/examproject/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
             session.setAttribute("isLoggedIn", true);
             session.setAttribute("userEmail", userEmail);
             session.setAttribute("userID", userService.findUserById(userEmail));
-            return "redirect:/projectList";
+            return "redirect:/login";
         } else {
             redirectAttributes.addAttribute("error", true);
             return "redirect:";
